be518c666a6580cda6b6a790ae06365e685853dd
diams <diamondra@wylog.com>
2022-12-05 17:42:15 +0300

    css

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <meta charset="utf-8">
 <link rel="stylesheet" href="css/style.css">
 <link href="css/bootstrap.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
 
 
 <script src="js/jquery.ui.min.js" type="text/javascript"></script>
